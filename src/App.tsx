db0529503f7d331c87b7a1e0bed1964371dfdbe8
Tushar Patel <83866888+Tusharpatel26@users.noreply.github.com>
2026-02-08 16:21:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/valentine-s-quest/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
